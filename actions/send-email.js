f9529d075d60323adcbdba3a4e7dd1af60bf25f3
SantanuOjha <145149337+SantanuOjha@users.noreply.github.com>
2025-03-02 04:33:03 +0530

    send-email.js

--- actions/send-email.js
+++ actions/send-email.js
@@ -1,34 +1,21 @@
-
 "use server";
 
 import { Resend } from "resend";
 
 export async function sendEmail({ to, subject, react }) {
-  // Initialize the Resend instance with your API key
   const resend = new Resend(process.env.RESEND_API_KEY || "");
 
-  if (!to || !subject || !react) {
-    console.error("Missing required parameters: to, subject, or react.");
-    return { success: false, error: "Missing required parameters." };
-  }
-
   try {
-    // Use the Resend instance to send an email
     const data = await resend.emails.send({
-      from: '<FINOVA@resend.dev>',
+      from: "Finance App <onboarding@resend.dev>",
       to,
       subject,
-      react,  // React email component if you're using one
+      react,
     });
 
-    console.log('Email sent successfully:', data);
     return { success: true, data };
   } catch (error) {
-    console.error('Failed to send email:', {
-      message: error.message,
-      stack: error.stack,
-      response: error.response ? error.response.data : null,
-    });
-    return { success: false, error: error.message };
+    console.error("Failed to send email:", error);
+    return { success: false, error };
   }
 }
